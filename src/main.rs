c243c43580b29c0c777d54776a6775189891c008
Peter Bowman <90658565+piderking@users.noreply.github.com>
2025-04-18 16:53:36 +0000

    Delete main/rsc

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-pub fn merge(data: Vec<Vec<i32>>) -> Vec<i32> {
-    let mut d = data.iter().flatten().map(|f| *f).collect::<Vec<i32>>();
-    d.sort();
-    d
-}
-pub fn main() {
-    let d = vec![vec![1, 2, 5], vec![2, 3, 6]];
-    println!("{:?}", merge(d));
-}
