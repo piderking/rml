f57b9a5a6f53f66b03e7ca576627f2dd5a26e73e
Peter Bowman <90658565+piderking@users.noreply.github.com>
2025-04-23 18:50:19 +0000

    Itterator Revision

--- rml-data/src/tensor/iterator.rs
+++ rml-data/src/tensor/iterator.rs
@@ -5,12 +5,6 @@ use super::{
     tensorable::Tensorable,
 };
 
-pub struct MutTensorItterator<'a, T: Tensorable<'a>> {
-    curr: usize,
-    tensor: Vec<T>,
-    phn: PhantomData<&'a T>,
-}
-
 pub struct TensorItterator<'a, T: Tensorable<'a>> {
     curr: usize,
     tensor: Vec<T>, // Convert Tensor into Vec[]
@@ -22,8 +16,34 @@ impl<'a, T: Tensorable<'a>> From<Tensor<'a, T>> for Vec<T> {
         value.into()
     }
 }
-// impl Itterator
 
+impl<'a, T: Tensorable<'a>> IntoIterator for &'a Tensor<'a, T> {
+    type Item = T;
+
+    type IntoIter = TensorItterator<'a, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        TensorItterator {
+            curr: 0,
+            tensor: self.to_vec(),
+            phn: PhantomData,
+        }
+    }
+}
+// impl Itterator
+impl<'a, T: Tensorable<'a>> Iterator for TensorItterator<'a, T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.curr < self.tensor.len() {
+            true => {
+                self.curr += 1;
+                Option::Some(self.tensor.get(self.curr - 1).unwrap().clone())
+            }
+            false => Option::None,
+        }
+    }
+}
 // Convert from any trait object into TensorWrapper()
 /*
 impl<T: Tensorable> From<Box<dyn TensorBound<T = T>>> for Tensor<T> {
