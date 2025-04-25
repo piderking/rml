fbe90969c3e425e12022ae3e1f5b37c8182e7455
Peter Bowman <90658565+piderking@users.noreply.github.com>
2025-04-25 16:47:33 +0000

    ops mod

--- rml-data/src/tensor/ops/mod.rs
+++ rml-data/src/tensor/ops/mod.rs
@@ -1,11 +1,2 @@
-use std::ops::Add;
-
-use super::{tensor::Tensor, tensorable::Tensorable};
-
-impl<T: Tensorable> Add<T> for Tensor<T> {
-    type Output = Tensor<T>;
-
-    fn add(self, rhs: T) -> Self::Output {
-        self.
-    }
-}
+mod basic;
+mod tensor;
