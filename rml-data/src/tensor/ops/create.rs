66e4ccf5e72feed99a7d8b835807756a0cf28aff
Peter Bowman <90658565+piderking@users.noreply.github.com>
2025-05-05 20:07:02 +0000

    Sigmoid impl

--- rml-data/src/tensor/ops/create.rs
+++ rml-data/src/tensor/ops/create.rs
@@ -63,6 +63,31 @@ use std::f32::consts::E;
 
 ops!(trait Sigmoid (Mul, Div), (
     fn sigmoid(&self){
-        Self::from_f32((1.0)/(1.0+E.powf(-1.0*self.clone().as_f32())))
+        Self::from_f32((1.0)/(1.0+E.powf(-1.0*self.as_f32())))
     })
 );
+
+ops!(trait Relu (), (
+    fn relu(&self){
+        if self.as_f32() > 0.0 {
+            self.clone()
+        } else {
+            Self::from_f32(0.0)
+        }
+    })
+);
+
+//(e^x - e^-x) / (e^x + e^-x)
+ops!(trait TanH (), (
+    fn tanh(&self){
+        let t = self.clone().as_f32();
+        Self::from_f32((E.powf(t) - E.powf(-1.0 * t))/(E.powf(t) + E.powf(-1.0 * t)))
+    })
+);
+
+// Other Impl
+impl Sigmoid for i32 {
+    fn from_f32(t: f32) -> Self {
+        t as i32
+    }
+}
--- rml-data/src/tensor/ops/dtype.rs
+++ rml-data/src/tensor/ops/dtype.rs
@@ -1,7 +1,6 @@
 use std::ops::{Add, Div, Mul, Sub};
 
-
-
+use super::create::Sigmoid;
 
 #[allow(non_camel_case_types)]
 pub trait dtypeops: Clone
@@ -11,25 +10,25 @@ where
     Self::Input: Sub<Self::Input, Output = Self::Input>,
     Self::Input: Mul<Self::Input, Output = Self::Input>,
     Self::Input: Div<Self::Input, Output = Self::Input>,
+    Self::Input: Sigmoid,
 {
     type Input;
 }
 
 macro_rules! req_ops {
     (impl ($tr:ty) for ($($type:ty),*)) => {
-        $(impl $tr for $type where 
+        $(impl $tr for $type where
             $type: Add<$type, Output = $type>,
             $type: Sub<$type, Output = $type>,
             $type: Mul<$type, Output = $type>,
             $type: Div<$type, Output = $type>,
-            $type: Clone 
+            $type: Sigmoid,
+            $type: Clone
         {
             type Input = $type;
         })*
     };
-    
+
 }
 
 req_ops!(impl (dtypeops) for (i32, f32));
-
-
