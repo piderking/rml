478a1317b615b72c937afcd4acbfbd196c55d058
Peter Bowman <90658565+piderking@users.noreply.github.com>
2025-05-19 15:31:50 +0000

    math tests

--- rml-math/src/lib.rs
+++ rml-math/src/lib.rs
@@ -1,6 +1,4 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub mod function;
 
 #[cfg(test)]
 mod tests {
@@ -8,7 +6,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        assert_eq!(4, 4);
     }
 }
