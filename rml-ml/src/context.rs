67ce839cc6bf8a17d982938cadc9b40af6af5869
Peter Bowman <90658565+piderking@users.noreply.github.com>
2025-05-29 16:10:34 +0000

    context state tmapping

--- rml-ml/src/context.rs
+++ rml-ml/src/context.rs
@@ -1,18 +1,27 @@
 //pub trait Context {}
 
-use rml_data::tensor::shape::tensor::Tensor;
+use rml_data::tensor::{
+    shape::tensor::Tensor,
+    traits::{dtype::dtype, tensor::TensorBound},
+};
 
 use crate::layers::{self, create::Layer};
 
-pub trait ContextFlag {
-    fn next(&mut self) -> ();
+pub trait ContextFlag<'a> {
+    type Output: TensorBound;
+    fn next(&mut self) -> Option<Self::Output>;
 }
 pub struct Input {}
 
 pub enum ContextState<'a> {
     Input(Tensor<'a, f32>),
-    Layer(Box<dyn Layer<'a, f32, Output = Tensor<'a, f32>>>),
-    Output(Tensor<'a, f32>),
+    Layer(
+        (
+            Box<dyn Layer<'a, f32, Output = Tensor<'a, f32>>>,
+            &'a ContextStruct<'a>,
+        ),
+    ),
+    Output(),
 }
 
 pub struct ContextStruct<'a> {
@@ -29,22 +38,37 @@ impl<'a> ContextStruct<'a> {
     }
 }
 
-impl<'a> ContextFlag for ContextStruct<'a> {
-    fn next(&mut self) -> () {
+impl<'a> ContextFlag<'a> for ContextStruct<'a> {
+    type Output = Tensor<'a, f32>;
+    fn next(&mut self) -> Option<Self::Output> {
         match self.layers.pop() {
             Some(prev) => match self.layers.get_mut(0) {
                 Some(layer) => match (prev, layer) {
-                    (ContextState::Input(n), ContextState::Layer(a)) => todo!(),
-                    (ContextState::Input(n), ContextState::Output(a)) => todo!(),
+                    (ContextState::Input(n), ContextState::Layer((a, ctx))) => {
+                        a.fill(n);
+                        a.layer(ctx);
+                        Option::None
+                    }
+                    (ContextState::Input(n), ContextState::Output()) => {
+                        // TODO --> Return Here
+                        Option::Some(n)
+                    }
                     (ContextState::Input(n), _) => panic!("Only 1 Input!"),
-                    (ContextState::Layer(n), ContextState::Layer(a)) => todo!(),
-                    (ContextState::Layer(n), ContextState::Output(a)) => todo!(),
+                    (ContextState::Layer((n1, t1)), ContextState::Layer((n2, _t2))) => {
+                        n2.fill(n1.layer(t1));
+                        Option::None
+                    }
+                    (ContextState::Layer((n, t)), ContextState::Output()) => {
+                        Option::Some(n.layer(t))
+                    }
                     (_, ContextState::Input(ns)) => panic!("Input must only be first!"),
-                    (ContextState::Output(n), _) => panic!("Out must be last"),
+                    (ContextState::Output(), _) => panic!("Out must be last"),
                 },
-                None => (),
+                None => {
+                    panic!("How did you get here? Like holy shit")
+                }
             },
-            None => (),
-        };
+            None => panic!("Context Must Include an Output"),
+        }
     }
 }
